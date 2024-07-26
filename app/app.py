02642f414667bd04839d6265e13ca42d61936e1d
ibrahimfnk <ibrahimkadiri.uae@gmail.com>
2024-07-26 15:50:50 +0530

    added about route, made changes to index, login and register. added css static files

--- app/app.py
+++ app/app.py
@@ -14,8 +14,25 @@ mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    else:
+        return render_template('index.html')
+    
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
